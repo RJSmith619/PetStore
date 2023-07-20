aecad7ce1dc6cf2b2448c29173c49624f3fb70b3
Jacob <jacobsmith619@gmail.com>
2023-07-20 14:30:57 -0400

    Adding WHILE logic

--- PetStore/Program.cs
+++ PetStore/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+Console.WriteLine("Press 1 to add a product");
+Console.WriteLine("Type 'exit' to quit");
+
+string userInput = Console.ReadLine();
+
+while(userInput.ToLower() != "exit")
+{
+    if(int.Parse(userInput) == 1)
+    {
+        Console.WriteLine("Testing");
+    }
+    Console.WriteLine("Press 1 to add a product");
+    Console.WriteLine("Type 'exit' to quit");
+    userInput = Console.ReadLine();
+
+}
