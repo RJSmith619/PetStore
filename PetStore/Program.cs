100cbbcc57f2f43563e2ca5cf3549658b680eab6
Jacob Smith <jacobsmith619@gmail.com>
2023-07-24 14:27:54 -0400

    Updated Program.cs WHILE logic

--- PetStore/Program.cs
+++ PetStore/Program.cs
@@ -20,7 +20,17 @@ while(userInput.ToLower() != "exit")
         Console.WriteLine("Please enter the weight in pounds: ");
         food.WeightPounds = int.Parse(Console.ReadLine());
 
+        Console.WriteLine("Is this kitten food? Please type 'yes' or 'no'. ");
+        if (Console.ReadLine().ToLower() == "yes")
+        {
+            food.KittenFood = true;
+        }
+        else
+        {
+            food.KittenFood = false;
+        }
 
+        Console.WriteLine("Weight in pounds: {0}\nKitten Food: {1}", food.WeightPounds, food.KittenFood);
     }
     Console.WriteLine("Press 1 to add a product");
     Console.WriteLine("Type 'exit' to quit");
